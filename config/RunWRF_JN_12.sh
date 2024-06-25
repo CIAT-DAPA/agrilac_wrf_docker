05dca163feeb0e817b12e39b7e98013fd23a7fbc
santiagoC <s.calderon@cgiar.org>
2024-06-24 23:54:28 -0500

    fixed error

--- config/RunWRF_JN_12.sh
+++ config/RunWRF_JN_12.sh
@@ -285,7 +285,7 @@ cp -r /home/WRF/SALIDAS_MAPAS-12 $OUTPUT_PATH"/grads"
 
 wrf_postprocessing -i $OUTPUT_PATH"/wrf" -o $OUTPUT_PATH"/postprocessing"
 
-python /home/email.py
+python /home/send_email.py
 
 exit
 
